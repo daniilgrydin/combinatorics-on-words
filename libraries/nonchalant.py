53d6a02b53b57d9ad4b7fec1dceb4f0d6bd65411
Daniil Grydin <61776829+daniilgrydin@users.noreply.github.com>
2026-05-20 13:39:25 -0700

    forgot the nonchalant construction

--- libraries/nonchalant.py
+++ libraries/nonchalant.py
@@ -1,3 +1,20 @@
 def generate_nonchalant_sequence(length, alphabet="abc"):
     for _ in range(length):
         pass
+    
+# Generates square-free words called nonchalant words in a greedy way. 
+def nonchalantWords(n,A):
+    from .square import is_square_free
+    result = ['1']
+
+    def greedyAdd(w):
+        for i in range(len(w),-1,-1):
+            for a in A:
+                if is_square_free(w[:i] + a + w[i:]):
+                    return w[:i] + a + w[i:]
+        return ""
+
+    for i in range(1,n+1):
+        result.append(greedyAdd(result[-1]))
+    
+    return result
