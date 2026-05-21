af3192c69c7dc2587096c57c9a036de2e4df6815
GoodOlLeifP <leifpetersen4579@gmail.com>
2026-05-21 13:34:10 -0700

    All Cycles Function

--- MR2020/proofs.py
+++ MR2020/proofs.py
@@ -2,6 +2,14 @@ from libraries.square import *
 from libraries.word import *
 import itertools as iter
 
+def verify_theorem_1_1():
+    A_finite = [25,41,48,50,63,71,72,77,79,81,83,84,85]
+    for i in range(1,87):
+        if not i in A_finite:
+            print("Looking for square-free ")
+            is_language_square_free(generate_square_free_words(i,3))
+    print("Done")
+
 def verify_theorem_2_3(A, substitution):
     # (I)
     V = generate_square_free_words(3, len(A))
